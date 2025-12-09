cb35d5e3ba590a97d98eb5c2f1b1b4d7e6fa3469
Karthi-8055 <vvce23cse0280@vvce.ac.in>
2025-12-09 23:28:24 +0530

    version 4

--- demo.java
+++ demo.java
@@ -3,6 +3,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("karthik");
         System.out.println("karthik");
-        System.out.println("karthik");
+        System.out.println("karthik gouda");
     }
 }
